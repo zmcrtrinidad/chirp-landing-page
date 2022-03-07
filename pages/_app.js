8f20cb856b651051bb3b198b7709950e39a9ce9b
zinia <zmcrtrinidad@neu.edu.ph>
2022-03-07 10:24:51 +0800

    navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+// _app.js
+
+import '@fortawesome/fontawesome-free/css/all.css';
+
+// ...
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
